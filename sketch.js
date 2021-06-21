761d3c5fc0c0b58477eee51bef1b2e162d3a0b40
klevvit <kle8it@gmail.com>
2021-06-21 19:25:20 +0300

    Animation for gentle image reveal

--- sketch.js
+++ sketch.js
@@ -1,43 +1,68 @@
 
 const IMG_WIDTH = 720;
 const IMG_HEIGHT = 1080;
-const POINTS_DIST = 46;
 
-const POINTS_X = Math.floor(IMG_WIDTH / POINTS_DIST);
-const POINTS_Y = Math.floor(IMG_HEIGHT / POINTS_DIST);
+let pointsDist = 72;
+let pointsDistDouble = pointsDist;
+let pointsX;
+let pointsY;
 
 let img;
 let points;
+let pointSize = Math.floor(pointsDist * 2**(1/2));
+
+function setPointsCntAndSize() {
+    pointsX = Math.floor(IMG_WIDTH / pointsDist);
+    pointsY = Math.floor(IMG_HEIGHT / pointsDist);
+    pointSize = Math.floor(pointsDist * 2**(1/2));
+}
 
 function preload() {
     img = loadImage('media/posvyat.jpg');
 }
 
-const pointSize = Math.floor(POINTS_DIST * 7 / 5);
 
 function setup() {
 
+    setPointsCntAndSize();
 
     createCanvas(IMG_WIDTH + Math.floor(pointSize / 2), IMG_HEIGHT + Math.floor(pointSize / 2));
-    // background(img);
-
-    img.loadPixels();
     
-    points = new Array(POINTS_X);
+    img.loadPixels();
+    frameRate(20);
+}
 
-    for (let i = 0; i < POINTS_X; i++) {
-        points[i] = new Array(POINTS_Y);
+
+function draw() {
+    
+    if (pointsDist > 7) {
+        makePoints();
+    } else {
+        background(255);
+        image(img, Math.floor(pointSize / 2), Math.floor(pointSize / 2));
+    }
+    
+}
+
+function makePoints() {
+
+    background(255);
+    
+    points = new Array(pointsX);
+    
+    for (let i = 0; i < pointsX; i++) {
+        points[i] = new Array(pointsY);
         
-        for (let j = 0; j < POINTS_Y; j++) {
+        for (let j = 0; j < pointsY; j++) {
             points[i][j] = { 
-                x: i * POINTS_DIST,
-                y: j * POINTS_DIST,
-                color: img.get(i * POINTS_DIST, j * POINTS_DIST)
+                x: i * pointsDist,
+                y: j * pointsDist,
+                color: img.get(i * pointsDist, j * pointsDist)
                 };
         }
-
+    
     }
-
+    
     strokeWeight(pointSize);
     
     for (let i = 0; i < points.length; i++) {
@@ -47,9 +72,8 @@ function setup() {
             point(p.x + Math.floor(pointSize / 2), p.y + Math.floor(pointSize / 2));
         }
     }
-
-}
-
-function draw() {
-
+    
+    pointsDistDouble = max(7, pointsDistDouble / 1.05);
+    pointsDist = Math.floor(pointsDistDouble);
+    setPointsCntAndSize();
 }
