d4576b3c11b5b741a7f2bf75f5196a605eadcc5f
klevvit <kle8it@gmail.com>
2021-06-30 22:26:38 +0300

    draw on mouse pressed Draw your painting above an image! Smart brush takes image's color right under your cursor. Also you can press 'F' to turn background image on or off.

--- sketch.js
+++ sketch.js
@@ -1,117 +1,161 @@
 
-const IMG_WIDTH = 576;
-const IMG_HEIGHT = 864;
-
-let pointsDist = 40;
-let pointsX;
-let pointsY;
-let pointSize;
-let lineWeight;
-
 let img;
-let points;
+const IMG_WIDTH = 576; // 616
+const IMG_HEIGHT = 864; // 904
 
+let lineWeight = 30;
+let shift = lineWeight / 2;  // shift for both axes to fit lines into canvas
 
-/* Matrices are usually arrays of rows, but in p5.js X coordinate is horizontal and Y is vertical. 
- * So it'll be a little tricky, because point[1][2] means Y = 1 and X = 2.
- */
-function setPointsCntAndSize() {
-    pointsX = Math.floor(1 + IMG_WIDTH / pointsDist);
-    pointsY = Math.floor(1 + IMG_HEIGHT / pointsDist);
-    pointSize = pointsDist / 3;
-    lineWeight = pointsDist / 6;
-}
+let backLayer;
+let lineLayer;
+
+let startX;
+let startY;
+let startColor;
+
+let drawMode = 'off';
+let showImageFlag = true;
+
 
 function preload() {
     img = loadImage('media/posvyat.jpg');
-    img.resize(IMG_WIDTH, IMG_HEIGHT);
 }
 
 
 function setup() {
-
-    setPointsCntAndSize();
+    
+    img.resize(IMG_WIDTH, IMG_HEIGHT);
     img.loadPixels();
 
-    let shift = max(pointSize / 2, lineWeight / 2);  // shift for both axes to fit points and lines into canvas
-
-    makePointsArray();
-    
-    createCanvas(IMG_WIDTH , IMG_HEIGHT);
+    createCanvas(IMG_WIDTH + 2 * shift, IMG_HEIGHT + 2 * shift);
     background(255);
-   
-    // ----- draw lines -----
-    strokeWeight(lineWeight);
 
-    // down-right
-    for (let i = 0; i < points.length - 1; i++) {
-        for (let j = 0; j < points[i].length; j++) {
-            if (j == pointsX - 1) 
-                continue;
-            let p1 = points[i][j];
-            let p2 = points[i + 1][j + i % 2];
-            gradiantLine(p1.color, p2.color, p1.x + shift, p1.y + shift, p2.x + shift, p2.y + shift);
-        }
-    }
+    backLayer = createGraphics(IMG_WIDTH + 2 * shift, IMG_HEIGHT + 2 * shift);
+    lineLayer = createGraphics(IMG_WIDTH + 2 * shift, IMG_HEIGHT + 2 * shift);
+    lineLayer.strokeWeight(lineWeight);
+}
 
-    // down-left
-    for (let i = 0; i < points.length - 1; i++) {
-        for (let j = 0; j < points[i].length; j++) {
-            if (j == 0 && i % 2 == 0) 
-                continue;
-            let p1 = points[i][j];
-            let p2 = points[i + 1][j - (i + 1) % 2];
-            gradiantLine(p1.color, p2.color, p1.x + shift, p1.y + shift, p2.x + shift, p2.y + shift);
-        }
-    }
+function mousePressed() {
 
-    // horizontal
-    for (let i = 0; i < points.length; i++) {
-        for (let j = 0; j < points[i].length - 1; j++) {
-            let p1 = points[i][j];
-            let p2 = points[i][j + 1];
-            gradiantLine(p1.color, p2.color, p1.x + shift, p1.y + shift, p2.x + shift, p2.y + shift);
-        }
-    }
+    if (!cursorInBounds())
+        return;
 
-    // ----! draw lines -----
-    
-    // ----- draw points -----
-    strokeWeight(pointSize);
+    startX = mouseX;
+    startY = mouseY;
+    startColor = getImageColor(startX, startY);
 
-    for (let i = 0; i < points.length; i++) {
-        for (let j = 0; j < points[i].length; j++) {
-            let p = points[i][j];
-            stroke(p.color);
-            point(p.x + shift, p.y + shift);
-        }
-    }
-    // ----! draw points -----
+    drawMode = 'paint';
 
+    loop();
 }
 
 
-function makePointsArray() {
-    points = new Array(pointsY);
+function mouseReleased() {
     
-    for (let i = 0; i < pointsY; i++) {
-        points[i] = new Array(pointsX - i % 2);
+    backLayer.image(lineLayer, 0, 0);
+    lineLayer.clear();
+    
+    drawMode = 'off';
+}
+
+
+function draw() {
+
+    switch (drawMode) {
+        case 'paint':
+
+            if (!cursorInBounds())
+                break;
         
-        for (let j = 0; j < pointsX - i % 2; j++) {
-            points[i][j] = { 
-                x: (j + 1/2 * (i % 2)) * pointsDist,
-                y: i * pointsDist,
-                color: color(img.get(j * pointsDist, i * pointsDist))
-                };
-        }
+            currentColor = getImageColor(mouseX, mouseY);
     
+            if (mouseX == startX && mouseY == startY) {
+    
+                lineLayer.stroke(startColor);
+                lineLayer.point(startX, startY);
+    
+            } else {
+    
+                gradiantLine(startColor, currentColor, startX, startY, mouseX, mouseY, lineLayer);
+            }
+        
+            startX = mouseX;
+            startY = mouseY;
+            startColor = currentColor;
+    
+            break;
+            
+        default:
+            break;
+    }
+
+    displayLayers(showImageFlag);
+}
+
+
+function displayLayers(showImage = true) {
+
+    background(255);
+    
+    if (showImage) {
+        
+        tint(255, 128);
+        image(img, shift, shift);
+        noTint();
+    }
+
+    image(backLayer, 0, 0);
+    image(lineLayer, 0, 0);
+}
+
+
+function keyTyped() {
+    switch (key) {
+        case 'F':
+        case 'f':
+            
+            showImageFlag = !showImageFlag;
+
+            break;
+    
+        default:
+            break;
     }
 }
 
-function gradiantLine(color1, color2, x1, y1, x2, y2) {
+
+/** Returns color of image point under coordinates (x, y) taking shift into account */
+function getImageColor(x, y) {
+    return color(img.get(x - shift, y - shift));
+}
+
+/** Returns true if cursor is at least at shift distance from all canvas sides, false otherwise. */
+function cursorInBounds() {
+    return mouseX >= shift && mouseY >= shift &&
+        mouseX <= IMG_WIDTH + shift && mouseY <= IMG_HEIGHT + shift;
+}
+
+// function makePointsArray() { 
+//     points = new Array(pointsY);
+    
+//     for (let i = 0; i < pointsY; i++) {
+//         points[i] = new Array(pointsX - i % 2);
+        
+//         for (let j = 0; j < pointsX - i % 2; j++) {
+//             points[i][j] = { 
+//                 x: (j + 1/2 * (i % 2)) * pointsDist,
+//                 y: i * pointsDist,
+//                 color: color(img.get(j * pointsDist, i * pointsDist))
+//                 };
+//         }
+    
+//     }
+// }
+
+function gradiantLine(color1, color2, x1, y1, x2, y2, layer) {
     for (let i = 0; i < 100; i ++) {
-      stroke(lerpColor(color1, color2, i/100.0));
-      line(
+      layer.stroke(lerpColor(color1, color2, i/100.0));
+      layer.line(
         ((100 - i) * x1 + i * x2) / 100.0,
         ((100 - i) * y1 + i * y2) / 100.0, 
         ((100 - i - 1) * x1 + (i + 1) * x2) / 100.0, 
